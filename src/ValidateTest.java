93e2f7edc86a0787c328b537b46cc6c682946633
blacktiger <matsuotaiga@mail.benesse.co.jp>
2023-07-14 15:40:21 +0900

    first commit

---
+++ src/ValidateTest.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ValidateTest {
+
+	public static void main(String[] args)
+	{
+		int validateCount = 0;
+		int totalCount = 0;
+		String filename = "src/NGWords.txt";
+
+        try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))){
+            String line;
+            while((line = in.readLine()) != null){
+            	totalCount++;
+            	if(!checkNickName(line)){
+            		System.out.println(line + "：NG!");
+            		validateCount++;
+            	}else{
+            		System.out.println(line + "：OK!");
+            	}
+            }
+
+            //結果表示
+            System.out.println("NGワード：" + validateCount + "/" + totalCount + ":" + validateCount*100/totalCount +"%");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            System.exit(-1); // 0 以外は異常終了
+        } catch (IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+	}
+
+	/*
+	 * return true:OKワード,false:NGワード
+	 * */
+	public static boolean checkNickName(String nickName){
+
+		boolean isOK = true;
+		//ここにNGワードを弾く処理を書く
+
+		return isOK;
+
+	}
+
+}
