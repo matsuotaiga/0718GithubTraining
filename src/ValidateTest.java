d1a183dda7b9e4b6cf11382d6991f5c98a097e6b
yosshi- <yosshi-@example.com>
2023-07-18 17:16:53 +0900

    mail, phoneNumber, zipCode Validation

--- src/ValidateTest.java
+++ src/ValidateTest.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class ValidateTest {
 
@@ -42,6 +43,51 @@ public class ValidateTest {
 
 		boolean isOK = true;
 		//ここにNGワードを弾く処理を書く
+
+
+		if(nickName == null || "".equals(nickName)) {
+			isOK = false;
+		}
+
+		if(nickName.length() >= 16) {
+			isOK = false;
+		}
+
+		// 郵便番号の正規表現パターン
+        String zipCodePatternString = "^\\d{3}-\\d{4}$";
+
+        // 正規表現パターンをコンパイル
+        Pattern zipCodePattern = Pattern.compile(zipCodePatternString);
+
+        // 郵便番号の形式がマッチするか判定
+        if (zipCodePattern.matcher(nickName).matches()) {
+            isOK = false;
+        }
+
+		// 電話番号の正規表現パターン
+        String phoneNumberPatternString = "^(\\d{2,4}-)?(\\d{2,4}-)?(\\d{4})$|^\\d{3}-\\d{3}-\\d{4}$|^\\d{10,11}$";
+
+        // 正規表現パターンをコンパイル
+        Pattern phoneNumberPattern = Pattern.compile(phoneNumberPatternString);
+
+        // 電話番号の形式がマッチするか判定
+        if (phoneNumberPattern.matcher(nickName).matches()) {
+            isOK = false;
+        }
+
+		// メールアドレスの正規表現パターン
+        String emailPatternString = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+        // 正規表現パターンをコンパイル
+        Pattern emailPattern = Pattern.compile(emailPatternString);
+
+        // メールアドレスの形式がマッチするか判定
+        if (emailPattern.matcher(nickName).matches()) {
+            isOK = false;
+        }
+
+		
+
 
 		return isOK;
 
